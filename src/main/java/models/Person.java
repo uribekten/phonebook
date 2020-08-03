822351a74a913805202ae31857146e483ba8b441
uribekten <uribekten@gmail.com>
2020-08-03 14:04:47 -0500

    phonebook app

---
+++ src/main/java/models/Person.java
@@ -0,0 +1,52 @@
+package models;
+
+public class Person {
+
+    private String ID;
+    private String firstName;
+    private String lastName;
+
+    public Person() {
+    }
+
+    public Person(String ID, String firstName, String lastName, String phoneNumber) {
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    private String phoneNumber;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ src/main/java/services/Service.java
@@ -0,0 +1,13 @@
+package services;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Service<T> {
+    Optional<T> getByID(String id);
+    Optional<T> getByNameAndSurname(String name, String surname);
+    List<T> searchByFilter(String filter);
+    long count();
+    void save(T t);
+    void delete(String id);
+}
